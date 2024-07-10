05c0f6b617d949d5ab67b462d75013614d88adb1
juhwan choi <choijuhwan8@gmail.com>
2024-07-10 03:32:40 +0000

    banner css fix

--- react-app/src/components/Banner.js
+++ react-app/src/components/Banner.js
@@ -1,49 +1,54 @@
-import { useState, useEffect } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import headerImg from '../assets/img/header-img.svg';
 
 export const Banner = () => {
-	const [loopNum, setLoopNum] = useState(0);
-	const [isDeleting, setIsDeleting] = useState(false);
-	const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
-	const [text, setText] = useState('');
-	const [delta, setDelta] = useState(300 - Math.random() * 100);
-	const period = 2000;
-	
-	useEffect(() => {
-		let ticker = setInterval(() => {
-			tick()
-		},delta)
-		return () => {clearInterval(ticker)};
-		}, [text])
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const period = 2000;
 
-	const tick = () => {
-		let i = loopNum % toRotate.length;
-		let fullText = toRotate[i];
-		let updatedText = isDeleting ? fullText.substring(0, text.length -1) : fullText.substring(0, text.length + 1);
-		
-		setText(updatedText);
-		
-		if (isDeleting) {
-			setDelta(prevDelta => prevDelta/2)
-		}
-		if (!isDeleting && updatedText === fullText) {
-			setIsDeleting(true);
-			setDelta(period);
-		} else if (isDeleting && updatedText === '') {
-			setIsDeleting(false);
-			setLoopNum(loopNum + 1);
-			setDelta(500);
-		}
-	}
+    const tick = useCallback(() => {
+        const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta / 2);
+        }
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        }
+    }, [loopNum, isDeleting, text, period]);
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta);
+
+        return () => {
+            clearInterval(ticker);
+        };
+    }, [delta, tick]); // Include 'tick' in the dependency array of useEffect
+
 	return (
 		<section className='banner' id='home'>
 			<Container>
 				<Row className="align-items-center">
 					<Col xs={12} md={6} xl={7}>
 						<span className="tagline">Welcome to my Portfolio</span>
-						<h1>{"Hi I'm webdecoded"}<span className="wrap">{text}</span></h1>
+						<h1>Hi I'm <span class="webdecoded">Choi Juhwan</span><br/><span class="wrap">{text}</span></h1>
 						<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
 						<button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25} /></button>
 					</Col>
