e87a0c6337e93027c25a5869fb2cce234689b43a
juhwan choi <choijuhwan8@gmail.com>
2024-07-12 03:11:32 +0000

    telegram icon added

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -51,7 +51,7 @@ export const NavBar = () => {
 				  <span className="navbar-text">
 					<div className="social-icon">
 						<a href="https://www.linkedin.com/in/juhwan-choi-861970219/" target='_blank' rel="noopener noreferrer"><img src={navIcon1} alt="linkedin icon" /></a>
-						<a href="a"><img src={navIcon2} alt="" /></a>
+						<a href="https://telegram.me/choi88888" target='_blank' rel="noopener noreferrer"><img src={navIcon2} alt="telegram icon" /></a>
 						<a href="https://www.instagram.com/ch_0i_" target='_blank' rel="noopener noreferrer"><img src={navIcon3} alt="instagram icon" /></a>
 					</div>
 					  {/* <HashLink to='#connect'>
